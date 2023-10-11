0ffc01b77e8fc6ecfc51665b6a1f894e41aecc7a
zz <zf@1.com>
2023-10-11 10:07:07 +0800

    1

---
+++ src/Comm/Comm.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Serv\Comm;
+
+class Comm
+{
+    const ACT_INSERT = 1;
+    const ACT_UPDATE = 2;
+    const ACT_DELETE = 3;
+    const ACT_SORT = 4; //改变顺序
+
+    public static function children($arr,$pidArr,&$result):void{
+        $tmp=[]; //存放每次找到的id,作为下一次的pid数组
+        foreach($arr as $v){
+            if(in_array($v->pid,$pidArr)){
+                $tmp[] = $v->id;
+            }
+        }
+        if(count($tmp)>0){
+            $result = array_merge($result,$tmp);
+            self::children($arr,$tmp,$result);
+        }
+    }
+
+    public static  function pager(&$itemList,$pageNum,$pageSize,$totalCount):array{
+        $maxPage = ceil($totalCount/$pageSize);
+        return [
+            'itemList'=>$itemList,
+            'pageNum'=> $pageNum,
+            'pageSize'=>$pageSize,
+            'totalCount' =>$totalCount,
+            'maxPage'=>$maxPage
+        ];
+    }
+
+}
